2f8e504b5cba06d828c904b0945bbf6b4a962ea4
Diego Haram <diegovazquezso@live.com.mx>
2018-07-18 23:06:46 -0500

    Initial commit

---
+++ MailerPHP/mailer.php
@@ -0,0 +1,27 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require './src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+$direccion = $_POST['email'];
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['message'];
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'diegoharamvazquezsolis@gmail.com';
+$mail->Password = 'peacesells';
+$mail->SetFrom('diegoharamvazquezsolis@gmail.com');
+$mail->Subject = 'Mensaje de: '.$nombre;
+$mail->Body = $mensaje."<br/> <h1>Contacto :".$direccion."</h1>";
+$mail->AddAddress('diegoharamvazquezsolis@gmail.com');
+$mail->Send();
+header("location: ../thanks.html");
+?>
